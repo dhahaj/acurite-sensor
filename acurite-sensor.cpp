f1a15b767eec6cb68c0da2b65effaeee9088bb61
Daniel Hahaj <dhahaj@gmail.com>
2024-05-02 22:06:28 -0500

    not working

--- acurite-sensor.cpp
+++ acurite-sensor.cpp
@@ -1,31 +1,32 @@
-#include "rfsensor.h"
+#include "acurite-sensor.h"
+#include "esphome/components/wifi/wifi_component.h"
 
-RFSensor *RFSensor::instance = nullptr; // Initialize static instance pointer
+// RFSensor *RFSensor::instance = nullptr; // Initialize static instance pointer
 
 // detect if a sync signal is present
-bool RFSensor::isSync(unsigned int idx)
-{
-    // check if we've received 4 squarewaves of matching timing
-    int i;
-    for (i = 0; i < 8; i += 2)
-    {
-        unsigned long t1 = timings[(idx + RING_BUFFER_SIZE - i) % RING_BUFFER_SIZE];
-        unsigned long t0 = timings[(idx + RING_BUFFER_SIZE - i - 1) % RING_BUFFER_SIZE];
-        if (t0 < (SYNC_HIGH - 100) || t0 > (SYNC_HIGH + 100) ||
-            t1 < (SYNC_LOW - 100) || t1 > (SYNC_LOW + 100))
-        {
-            return false;
-        }
-    }
+// bool RFSensor::isSync(unsigned int idx)
+// {
+//     // check if we've received 4 squarewaves of matching timing
+//     int i;
+//     for (i = 0; i < 8; i += 2)
+//     {
+//         unsigned long t1 = timings[(idx + RING_BUFFER_SIZE - i) % RING_BUFFER_SIZE];
+//         unsigned long t0 = timings[(idx + RING_BUFFER_SIZE - i - 1) % RING_BUFFER_SIZE];
+//         if (t0 < (SYNC_HIGH - 100) || t0 > (SYNC_HIGH + 100) ||
+//             t1 < (SYNC_LOW - 100) || t1 > (SYNC_LOW + 100))
+//         {
+//             return false;
+//         }
+//     }
 
-    // check if there is a long sync period prior to the 4 squarewaves
-    unsigned long t = timings[(idx + RING_BUFFER_SIZE - i) % RING_BUFFER_SIZE];
-    if (t < (SYNC_LENGTH - 400) || t > (SYNC_LENGTH + 400) || digitalRead(DATAPIN) != HIGH)
-    {
-        return false;
-    }
-    return true;
-}
+//     // check if there is a long sync period prior to the 4 squarewaves
+//     unsigned long t = timings[(idx + RING_BUFFER_SIZE - i) % RING_BUFFER_SIZE];
+//     if (t < (SYNC_LENGTH - 400) || t > (SYNC_LENGTH + 400) || digitalRead(DATAPIN) != HIGH)
+//     {
+//         return false;
+//     }
+//     return true;
+// }
 
 int RFSensor::t2b(unsigned int t0, unsigned int t1)
 {
@@ -42,25 +43,29 @@ int RFSensor::t2b(unsigned int t0, unsigned int t1)
     return -1; // undefined
 }
 
+// unsigned long RFSensor::extractData(unsigned int syncIndex, unsigned int bits, unsigned int startPos, unsigned int size)
+// {
+//     unsigned int startIndex, stopIndex;
+//     unsigned long value = 0;
+//     bool fail = false;
+//     startIndex = (syncIndex + (bits * 8 + startPos) * 2) % RING_BUFFER_SIZE;
+//     stopIndex = (syncIndex + (bits * 8 + size) * 2) % RING_BUFFER_SIZE;
 
-unsigned long RFSensor::extractData(unsigned int syncIndex, unsigned int bits, unsigned int startPos, unsigned int size)
+//     for (int i = startIndex; i != stopIndex; i = (i + 2) % RING_BUFFER_SIZE)
+//     {
+//         int bit = t2b(timings[i], timings[(i + 1) % RING_BUFFER_SIZE]);
+//         value = (value << 1) + bit;
+//         if (bit < 0)
+//             fail = true;
+//     }
+
+//     if (fail)
+//         return -1;
+
+//     return value;
+// }
+
+bool RFSensor::check_wifi_status()
 {
-    unsigned int startIndex, stopIndex;
-    unsigned long value = 0;
-    bool fail = false;
-    startIndex = (syncIndex + (bits * 8 + startPos) * 2) % RING_BUFFER_SIZE;
-    stopIndex = (syncIndex + (bits * 8 + size) * 2) % RING_BUFFER_SIZE;
-
-    for (int i = startIndex; i != stopIndex; i = (i + 2) % RING_BUFFER_SIZE)
-    {
-        int bit = t2b(timings[i], timings[(i + 1) % RING_BUFFER_SIZE]);
-        value = (value << 1) + bit;
-        if (bit < 0)
-            fail = true;
-    }
-
-    if (fail)
-        return -1;
-
-    return value;
+    return esphome::wifi::global_wifi_component->is_connected();
 }
--- acurite-sensor.h
+++ acurite-sensor.h
@@ -1,7 +1,9 @@
-#ifndef RFSENSOR_H
-#define RFSENSOR_H
+#pragma once
 
 #include "esphome.h"
+#include "esphome/core/log.h"
+#include "esphome/core/component.h"
+#include "esphome/components/wifi/wifi_component.h"
 
 #define RING_BUFFER_SIZE 256
 
@@ -13,7 +15,7 @@
 #define BIT1_LOW 220
 #define BIT0_HIGH 220
 #define BIT0_LOW 400
-
+#define BUFFER_SIZE 512
 #define DATAPIN 14
 #define LEDPIN 2
 
@@ -30,34 +32,112 @@ private:
   volatile unsigned int syncIndex2 = 0; // index of the second sync signal
   volatile bool received = false;
   volatile bool interruptFlag = false;
-  static RFSensor *instance; // Static instance pointer
+  volatile unsigned long _temp, _hum;
+  char buffer[BUFFER_SIZE];
+  uint32_t lastTime = 0;
+  bool flag = true;
+
+  // static RFSensor *instance; // Static instance pointer
 
   /// @brief Interrupt handler
-  static void IRAM_ATTR interrupt_handler()
-  {
-    if (instance)
-    {
-      instance->handle_interrupt();
-    }
-  }
+  // static void IRAM_ATTR interrupt_handler()
+  // {
+  //   if (instance)
+  //   {
+  //     instance->handle_interrupt();
+  //   }
+  // }
 
   /// @brief Handle interrupt
-  void handle_interrupt()
-  {
-    static unsigned long lastTime = 0;
-    static unsigned int ringIndex = 0;
-    static unsigned int syncCount = 0;
+  // void handle_interrupt()
+  // {
+  //   static unsigned long lastTime = 0;
+  //   static unsigned int ringIndex = 0;
+  //   static unsigned int syncCount = 0;
 
+  //   // ignore if we haven't processed the previous received signal
+  //   if (received)
+  //     return;
+
+  //   interruptFlag = true;
+
+  //   // calculating timing since last change
+  //   unsigned long time = micros();
+  //   unsigned long duration = time - lastTime;
+  //   lastTime = time;
+
+  //   // store data in ring buffer
+  //   ringIndex = (ringIndex + 1) % RING_BUFFER_SIZE;
+  //   timings[ringIndex] = duration;
+
+  //   // detect sync signal
+  //   if (isSync(ringIndex))
+  //   {
+  //     syncCount++;
+  //     // first time sync is seen, record buffer index
+  //     if (syncCount == 1)
+  //     {
+  //       syncIndex1 = (ringIndex + 1) % RING_BUFFER_SIZE;
+  //     }
+  //     else if (syncCount == 2)
+  //     {
+  //       // second time sync is seen, start bit conversion
+  //       syncCount = 0;
+  //       syncIndex2 = (ringIndex + 1) % RING_BUFFER_SIZE;
+  //       unsigned int changeCount = (syncIndex2 < syncIndex1) ? (syncIndex2 + RING_BUFFER_SIZE - syncIndex1) : (syncIndex2 - syncIndex1);
+  //       // changeCount must be 122 -- 60 bits x 2 + 2 for sync
+  //       if (changeCount != 122)
+  //       {
+  //         received = false;
+  //         syncIndex1 = 0;
+  //         syncIndex2 = 0;
+  //       }
+  //       else
+  //       {
+  //         received = true;
+  //       }
+  //     }
+  //   }
+  //   interruptFlag = false;
+  // }
+
+public:
+  // bool isSync(unsigned int idx);
+
+  // detect if a sync signal is present
+  bool isSync(unsigned int idx)
+  {
+    // check if we've received 4 squarewaves of matching timing
+    int i;
+    for (i = 0; i < 8; i += 2)
+    {
+      unsigned long t1 = timings[(idx + RING_BUFFER_SIZE - i) % RING_BUFFER_SIZE];
+      unsigned long t0 = timings[(idx + RING_BUFFER_SIZE - i - 1) % RING_BUFFER_SIZE];
+      if (t0 < (SYNC_HIGH - 100) || t0 > (SYNC_HIGH + 100) || t1 < (SYNC_LOW - 100) || t1 > (SYNC_LOW + 100))
+        return false;
+    }
+
+    // check if there is a long sync period prior to the 4 squarewaves
+    unsigned long t = timings[(idx + RING_BUFFER_SIZE - i) % RING_BUFFER_SIZE];
+
+    if (t < (SYNC_LENGTH - 400) || t > (SYNC_LENGTH + 400) || digitalRead(DATAPIN) != HIGH)
+      return false;
+
+    return true;
+  }
+
+  IRAM_ATTR void ISR()
+  {
+    static unsigned long duration = 0, lastTime = 0;
+    static unsigned int ringIndex = 0, syncCount = 0;
 
     // ignore if we haven't processed the previous received signal
-    if (received)
+    if (received == true)
       return;
 
-    interruptFlag = true;
-    
     // calculating timing since last change
-    unsigned long time = micros();
-    unsigned long duration = time - lastTime;
+    long time = micros();
+    duration = time - lastTime;
     lastTime = time;
 
     // store data in ring buffer
@@ -70,9 +150,7 @@ private:
       syncCount++;
       // first time sync is seen, record buffer index
       if (syncCount == 1)
-      {
         syncIndex1 = (ringIndex + 1) % RING_BUFFER_SIZE;
-      }
       else if (syncCount == 2)
       {
         // second time sync is seen, start bit conversion
@@ -87,58 +165,21 @@ private:
           syncIndex2 = 0;
         }
         else
-        {
           received = true;
-        }
       }
     }
-    interruptFlag = false;
   }
 
-public:
-  bool isSync(unsigned int idx);
-  int t2b(unsigned int t0, unsigned int t1);
-  unsigned long extractData(unsigned int syncIndex, unsigned int bits, unsigned int startPos, unsigned int size);
-
-  RFSensor()
+  void handleReceive()
   {
-    instance = this; // Set the instance pointer in the constructor
-  }
-
-  void setup() override
-  {
-    Serial.begin(115200);
-    Serial.println("RF Sensor setup");
-    pinMode(DATAPIN, INPUT);
-    pinMode(LEDPIN, OUTPUT);
-    digitalWrite(LEDPIN, LOW);
-
-    // delay(1000);
-
-    Serial.println("enabling interrupts...");
-    attachInterrupt(digitalPinToInterrupt(DATAPIN), RFSensor::interrupt_handler, CHANGE);
-  }
-
-  void loop() override
-  {
-    if (!received || interruptFlag)
-    {
-      return;
-    }
-
-    digitalWrite(LEDPIN, LOW);
-    Serial.println("Received data");
-
-    // disable interrupt to avoid new data corrupting the buffer
     detachInterrupt(digitalPinToInterrupt(DATAPIN));
-
+    // extract humidity value
+    unsigned long humidity, temp;
     unsigned int startIndex, stopIndex;
-    unsigned long humidity = 0;
     bool fail = false;
     startIndex = (syncIndex1 + (3 * 8 + 1) * 2) % RING_BUFFER_SIZE;
-    stopIndex = (syncIndex1 + (3 * 8 + 8) * 2) % RING_BUFFER_SIZE;
+    stopIndex =  (syncIndex1 + (3 * 8 + 8) * 2) % RING_BUFFER_SIZE;
 
-    // extract humidity value
     for (int i = startIndex; i != stopIndex; i = (i + 2) % RING_BUFFER_SIZE)
     {
       int bit = t2b(timings[i], timings[(i + 1) % RING_BUFFER_SIZE]);
@@ -147,36 +188,30 @@ public:
         fail = true;
     }
 
-    // publish humidity value
-    if (!fail)
-    {
-      humidity_sensor->publish_state(humidity);
-      Serial.print("Humidity: ");
-      Serial.println(humidity);
-    }
-    else
-    {
-      Serial.println("Decoding error.");
+
+    if (fail)
+      Serial.println(F("Decoding error."));
+    else {
+      Serial.print(F("Humidity: "));
+      Serial.print(humidity);
+      Serial.print("\% / ");
     }
 
-    // extract temperature value
-    unsigned long temp = 0;
     fail = false;
 
-    // // most significant 4 bits
+    // most significant 4 bits
     startIndex = (syncIndex1 + (4 * 8 + 4) * 2) % RING_BUFFER_SIZE;
-    stopIndex = (syncIndex1 + (4 * 8 + 8) * 2) % RING_BUFFER_SIZE;
+    stopIndex  = (syncIndex1 + (4 * 8 + 8) * 2) % RING_BUFFER_SIZE;
     for (int i = startIndex; i != stopIndex; i = (i + 2) % RING_BUFFER_SIZE)
     {
       int bit = t2b(timings[i], timings[(i + 1) % RING_BUFFER_SIZE]);
       temp = (temp << 1) + bit;
-      if (bit < 0)
-        fail = true;
+      if (bit < 0)  fail = true;
     }
 
-    // // least significant 7 bits
+    // least significant 7 bits
     startIndex = (syncIndex1 + (5 * 8 + 1) * 2) % RING_BUFFER_SIZE;
-    stopIndex = (syncIndex1 + (5 * 8 + 8) * 2) % RING_BUFFER_SIZE;
+    stopIndex  = (syncIndex1 + (5 * 8 + 8) * 2) % RING_BUFFER_SIZE;
     for (int i = startIndex; i != stopIndex; i = (i + 2) % RING_BUFFER_SIZE)
     {
       int bit = t2b(timings[i], timings[(i + 1) % RING_BUFFER_SIZE]);
@@ -185,31 +220,164 @@ public:
         fail = true;
     }
 
-    // // publish temperature value
-    if (!fail)
-    {
-      unsigned long result = (int)((temp - 1024) / 10 + 1.9 + 0.5);
-      temperature_sensor->publish_state(result);
-
-      Serial.print("Temperature: ");
-      Serial.print(result);
-      Serial.println("°C");
-    }
+    if (fail)
+      Serial.println(F("Decoding error."));
     else
     {
-      Serial.println("Decoding error.");
+      Serial.print(F("Temperature: "));
+      Serial.print((int)((temp - 1024) / 10 + 1.9 + 0.5)); // round to the nearest integer
+      Serial.write(176);    // degree symbol
+      Serial.print(F("C/"));
+      Serial.print((int)(((temp - 1024) / 10 + 1.9 + 0.5) * 9 / 5 + 32)); // convert to F
+      Serial.write(176);    // degree symbol
+      Serial.println(F("F"));
     }
 
+    // delay for 1 second to avoid repetitions
+    delay(1000);
     received = false;
     syncIndex1 = 0;
     syncIndex2 = 0;
 
-    digitalWrite(LEDPIN, HIGH);
-
     // re-enable interrupt
-    attachInterrupt(digitalPinToInterrupt(DATAPIN), RFSensor::interrupt_handler, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(DATAPIN), RFSensor::ISR, CHANGE);
+
+  }
+  
+  // detect if a sync signal is present
+  // bool isSync(unsigned int idx)
+  // {
+  //   // check if we've received 4 squarewaves of matching timing
+  //   int i;
+  //   for (i = 0; i < 8; i += 2)
+  //   {
+  //     unsigned long t1 = timings[(idx + RING_BUFFER_SIZE - i) % RING_BUFFER_SIZE];
+  //     unsigned long t0 = timings[(idx + RING_BUFFER_SIZE - i - 1) % RING_BUFFER_SIZE];
+  //     if (t0 < (SYNC_HIGH - 100) || t0 > (SYNC_HIGH + 100) || t1 < (SYNC_LOW - 100) || t1 > (SYNC_LOW + 100))
+  //       return false;
+  //   }
+
+  //   // check if there is a long sync period prior to the 4 squarewaves
+  //   unsigned long t = timings[(idx + RING_BUFFER_SIZE - i) % RING_BUFFER_SIZE];
+
+  //   if (t < (SYNC_LENGTH - 400) || t > (SYNC_LENGTH + 400) || digitalRead(DATAPIN) != HIGH)
+  //     return false;
+
+  //   return true;
+  // }
+
+  int t2b(unsigned int t0, unsigned int t1);
+  // unsigned long extractData(unsigned int syncIndex, unsigned int bits, unsigned int startPos, unsigned int size);
+  bool check_wifi_status(void);
+
+  // RFSensor()
+  // {
+  //   instance = this; // Set the instance pointer in the constructor
+  // }
+
+  void setup() override
+  {
+    Serial.begin(115200);
+    // pinMode(DATAPIN, INPUT);
+    // pinMode(LEDPIN, OUTPUT);
+    // digitalWrite(LEDPIN, LOW);
+
+    // attachInterrupt(digitalPinToInterrupt(DATAPIN), RFSensor::interrupt_handler, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(DATAPIN), RFSensor::ISR, CHANGE);
+  }
+
+  // bool connected = false;
+
+  void loop() override
+  {
+    if (received == true)
+    {
+      handleReceive();
+    }
+
+    // Checking WiFi status here to avoid entering a bootloop
+    // if (!check_wifi_status())
+    // {
+    //   return;
+    // }
+    // else if (!connected)
+    // {
+    //   Serial.println("WiFi Connected.");
+    //   attachInterrupt(digitalPinToInterrupt(DATAPIN), RFSensor::interrupt_handler, CHANGE); // attach the interrupt
+    //   connected = true;
+    // }
+
+    // if (!received || interruptFlag)
+    // {
+    //   return;
+    // }
+
+    // digitalWrite(LEDPIN, LOW);
+
+    // // disable interrupt to avoid new data corrupting the buffer
+    // detachInterrupt(digitalPinToInterrupt(DATAPIN));
+
+    // unsigned int startIndex, stopIndex;
+    // unsigned long humidity = 0;
+    // bool fail = false;
+    // startIndex = (syncIndex1 + (3 * 8 + 1) * 2) % RING_BUFFER_SIZE;
+    // stopIndex = (syncIndex1 + (3 * 8 + 8) * 2) % RING_BUFFER_SIZE;
+
+    // // extract humidity value
+    // for (int i = startIndex; i != stopIndex; i = (i + 2) % RING_BUFFER_SIZE)
+    // {
+    //   int bit = t2b(timings[i], timings[(i + 1) % RING_BUFFER_SIZE]);
+    //   humidity = (humidity << 1) + bit;
+    //   if (bit < 0)
+    //     fail = true;
+    // }
+
+    // // publish humidity value
+    // if (!fail)
+    // {
+    //   humidity_sensor->publish_state(humidity);
+    // }
+
+    // // extract temperature value
+    // unsigned long temp = 0;
+    // fail = false;
+
+    // // most significant 4 bits
+    // startIndex = (syncIndex1 + (4 * 8 + 4) * 2) % RING_BUFFER_SIZE;
+    // stopIndex = (syncIndex1 + (4 * 8 + 8) * 2) % RING_BUFFER_SIZE;
+    // for (int i = startIndex; i != stopIndex; i = (i + 2) % RING_BUFFER_SIZE)
+    // {
+    //   int bit = t2b(timings[i], timings[(i + 1) % RING_BUFFER_SIZE]);
+    //   temp = (temp << 1) + bit;
+    //   if (bit < 0)
+    //     fail = true;
+    // }
+
+    // // least significant 7 bits
+    // startIndex = (syncIndex1 + (5 * 8 + 1) * 2) % RING_BUFFER_SIZE;
+    // stopIndex = (syncIndex1 + (5 * 8 + 8) * 2) % RING_BUFFER_SIZE;
+    // for (int i = startIndex; i != stopIndex; i = (i + 2) % RING_BUFFER_SIZE)
+    // {
+    //   int bit = t2b(timings[i], timings[(i + 1) % RING_BUFFER_SIZE]);
+    //   temp = (temp << 1) + bit;
+    //   if (bit < 0)
+    //     fail = true;
+    // }
+
+    // // publish temperature value
+    // if (!fail)
+    // {
+    //   unsigned long result = (int)((temp - 1024) / 10 + 1.9 + 0.5);
+    //   temperature_sensor->publish_state(result);
+    // }
+
+    // received = false;
+    // syncIndex1 = 0;
+    // syncIndex2 = 0;
+
+    // digitalWrite(LEDPIN, HIGH);
+
+    // // re-enable interrupt
+    // attachInterrupt(digitalPinToInterrupt(DATAPIN), RFSensor::interrupt_handler, CHANGE);
   }
 };
-
-
-#endif // RFSENSOR_H
