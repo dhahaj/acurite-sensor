e32feaa236858344613c975ac1834e871b7f7c87
Daniel Hahaj <dhahaj@gmail.com>
2024-05-04 05:11:06 -0500

    updated

--- __init__.py
+++ __init__.py
@@ -1,44 +1,69 @@
-
 import esphome.codegen as cg
-from esphome.components import ble_client
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_THROTTLE
+from esphome import core
+from esphome.components import sensor
 
 CODEOWNERS = ["@dhahaj"]
 
 AUTO_LOAD = ["sensor"]
-MULTI_CONF = True
 
-CONF_HELTEC_BALANCER_BLE_ID = "heltec_balancer_ble_id"
+# This should match the class name in your C++ header
+AcuriteSensor = cg.global_ns.class_('AcuriteSensor', cg.Component)
 
-heltec_balancer_ble_ns = cg.esphome_ns.namespace("heltec_balancer_ble")
-HeltecBalancerBle = heltec_balancer_ble_ns.class_(
-    "HeltecBalancerBle", ble_client.BLEClientNode, cg.PollingComponent
-)
+CONFIG_SCHEMA = sensor.sensor_schema(AcuriteSensor).extend({
+    # Here you can define configuration parameters that are read from YAML
+    # For example, if your sensor supports configuring a measurement interval
+    cg.Optional('measurement_interval'): cv.positive_time_period_milliseconds,
+}).extend(cg.polling_component_schema('60s'))  # Default polling interval
 
-HELTEC_BALANCER_BLE_COMPONENT_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(CONF_HELTEC_BALANCER_BLE_ID): cv.use_id(HeltecBalancerBle),
-    }
-)
-
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(HeltecBalancerBle),
-            cv.Optional(
-                CONF_THROTTLE, default="2s"
-            ): cv.positive_time_period_milliseconds,
-        }
-    )
-    .extend(ble_client.BLE_CLIENT_SCHEMA)
-    .extend(cv.polling_component_schema("5s"))
-)
-
-
-async def to_code(config):
+def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await ble_client.register_ble_node(var, config)
+    if 'measurement_interval' in config:
+        cg.add(var.set_measurement_interval(config['measurement_interval']))
+    yield cg.register_component(var, config)
+    yield sensor.register_sensor(var, config)
 
-    cg.add(var.set_throttle(config[CONF_THROTTLE]))
+
+# import esphome.codegen as cg
+# from esphome.components import ble_client
+# import esphome.config_validation as cv
+# from esphome.const import CONF_ID, CONF_THROTTLE
+
+# CODEOWNERS = ["@dhahaj"]
+
+# AUTO_LOAD = ["sensor"]
+# MULTI_CONF = True
+
+# CONF_HELTEC_BALANCER_BLE_ID = "heltec_balancer_ble_id"
+
+# heltec_balancer_ble_ns = cg.esphome_ns.namespace("heltec_balancer_ble")
+# HeltecBalancerBle = heltec_balancer_ble_ns.class_(
+#     "HeltecBalancerBle", ble_client.BLEClientNode, cg.PollingComponent
+# )
+
+# HELTEC_BALANCER_BLE_COMPONENT_SCHEMA = cv.Schema(
+#     {
+#         cv.GenerateID(CONF_HELTEC_BALANCER_BLE_ID): cv.use_id(HeltecBalancerBle),
+#     }
+# )
+
+# CONFIG_SCHEMA = (
+#     cv.Schema(
+#         {
+#             cv.GenerateID(): cv.declare_id(HeltecBalancerBle),
+#             cv.Optional(
+#                 CONF_THROTTLE, default="2s"
+#             ): cv.positive_time_period_milliseconds,
+#         }
+#     )
+#     .extend(ble_client.BLE_CLIENT_SCHEMA)
+#     .extend(cv.polling_component_schema("5s"))
+# )
+
+
+# async def to_code(config):
+#     var = cg.new_Pvariable(config[CONF_ID])
+#     await cg.register_component(var, config)
+#     await ble_client.register_ble_node(var, config)
+
+#     cg.add(var.set_throttle(config[CONF_THROTTLE]))
--- acurite-sensor.h
+++ acurite-sensor.h
@@ -27,6 +27,21 @@ public:
   esphome::sensor::Sensor *temperature_sensor = new esphome::sensor::Sensor();
   esphome::sensor::Sensor *humidity_sensor = new esphome::sensor::Sensor();
 
+  bool connected = false;
+
+  bool isSync(unsigned int idx);
+  int timeToBits(unsigned int t0, unsigned int t1);
+  unsigned long extractData(unsigned int syncIndex, unsigned int bits, unsigned int startPos, unsigned int size);
+  bool check_wifi_status(void);
+  void setup(void) override;
+  void loop() override;
+  void handle_interrupt();
+
+  AcuriteSensor()
+  {
+    instance = this; // Set the instance pointer in the constructor
+  }
+
 private:
   volatile unsigned long timings[RING_BUFFER_SIZE];
   volatile unsigned int syncIndex1 = 0; // index of the first sync signal
@@ -44,168 +59,9 @@ private:
     }
   }
 
-  /// @brief Handle interrupt
-  void handle_interrupt()
-  {
-    static unsigned long lastTime = 0;
-    static unsigned int ringIndex = 0;
-    static unsigned int syncCount = 0;
+ 
 
-    // ignore if we haven't processed the previous received signal
-    if (received)
-      return;
-
-    interruptFlag = true;
-
-    // calculating timing since last change
-    unsigned long time = micros();
-    unsigned long duration = time - lastTime;
-    lastTime = time;
-
-    // store data in ring buffer
-    ringIndex = (ringIndex + 1) % RING_BUFFER_SIZE;
-    timings[ringIndex] = duration;
-
-    // detect sync signal
-    if (isSync(ringIndex))
-    {
-      syncCount++;
-      // first time sync is seen, record buffer index
-      if (syncCount == 1)
-      {
-        syncIndex1 = (ringIndex + 1) % RING_BUFFER_SIZE;
-      }
-      else if (syncCount == 2)
-      {
-        // second time sync is seen, start bit conversion
-        syncCount = 0;
-        syncIndex2 = (ringIndex + 1) % RING_BUFFER_SIZE;
-        unsigned int changeCount = (syncIndex2 < syncIndex1) ? (syncIndex2 + RING_BUFFER_SIZE - syncIndex1) : (syncIndex2 - syncIndex1);
-        // changeCount must be 122 -- 60 bits x 2 + 2 for sync
-        if (changeCount != 122)
-        {
-          received = false;
-          syncIndex1 = 0;
-          syncIndex2 = 0;
-        }
-        else
-        {
-          received = true;
-        }
-      }
-    }
-    interruptFlag = false;
-  }
-
-public:
-  bool isSync(unsigned int idx);
-  int timeToBits(unsigned int t0, unsigned int t1);
-  unsigned long extractData(unsigned int syncIndex, unsigned int bits, unsigned int startPos, unsigned int size);
-  bool check_wifi_status(void);
-
-  AcuriteSensor()
-  {
-    instance = this; // Set the instance pointer in the constructor
-  }
-
-  void setup() override
-  {
-    Serial.begin(115200);
-    pinMode(DATAPIN, INPUT);
-    pinMode(LEDPIN, OUTPUT);
-    digitalWrite(LEDPIN, LOW);
-
-    // attachInterrupt(digitalPinToInterrupt(DATAPIN), RFSensor::interrupt_handler, CHANGE);
-  }
-
-  bool connected = false;
-
-  void loop() override
-  {
-    if (!check_wifi_status())
-    {
-      return;
-    }
-    else if (!connected)
-    {
-      Serial.println("WiFi Connected.");
-      attachInterrupt(digitalPinToInterrupt(DATAPIN), AcuriteSensor::interrupt_handler, CHANGE);
-      connected = true;
-    }
-
-    if (!received || interruptFlag)
-    {
-      return;
-    }
-
-    digitalWrite(LEDPIN, LOW);
-
-    // disable interrupt to avoid new data corrupting the buffer
-    detachInterrupt(digitalPinToInterrupt(DATAPIN));
-
-    unsigned int startIndex, stopIndex;
-    unsigned long humidity = 0;
-    bool fail = false;
-    startIndex = (syncIndex1 + (3 * 8 + 1) * 2) % RING_BUFFER_SIZE;
-    stopIndex = (syncIndex1 + (3 * 8 + 8) * 2) % RING_BUFFER_SIZE;
-
-    // extract humidity value
-    for (int i = startIndex; i != stopIndex; i = (i + 2) % RING_BUFFER_SIZE)
-    {
-      int bit = timeToBits(timings[i], timings[(i + 1) % RING_BUFFER_SIZE]);
-      humidity = (humidity << 1) + bit;
-      if (bit < 0)
-        fail = true;
-    }
-
-    // publish humidity value
-    if (!fail)
-    {
-      humidity_sensor->publish_state(humidity);
-    }
-
-    // extract temperature value
-    unsigned long temp = 0;
-    fail = false;
-
-    // // most significant 4 bits
-    startIndex = (syncIndex1 + (4 * 8 + 4) * 2) % RING_BUFFER_SIZE;
-    stopIndex = (syncIndex1 + (4 * 8 + 8) * 2) % RING_BUFFER_SIZE;
-    for (int i = startIndex; i != stopIndex; i = (i + 2) % RING_BUFFER_SIZE)
-    {
-      int bit = timeToBits(timings[i], timings[(i + 1) % RING_BUFFER_SIZE]);
-      temp = (temp << 1) + bit;
-      if (bit < 0)
-        fail = true;
-    }
-
-    // // least significant 7 bits
-    startIndex = (syncIndex1 + (5 * 8 + 1) * 2) % RING_BUFFER_SIZE;
-    stopIndex = (syncIndex1 + (5 * 8 + 8) * 2) % RING_BUFFER_SIZE;
-    for (int i = startIndex; i != stopIndex; i = (i + 2) % RING_BUFFER_SIZE)
-    {
-      int bit = timeToBits(timings[i], timings[(i + 1) % RING_BUFFER_SIZE]);
-      temp = (temp << 1) + bit;
-      if (bit < 0)
-        fail = true;
-    }
-
-    // // publish temperature value
-    if (!fail)
-    {
-      unsigned long result = (int)((temp - 1024) / 10 + 1.9 + 0.5);
-      temperature_sensor->publish_state(result);
-    }
-
-    received = false;
-    syncIndex1 = 0;
-    syncIndex2 = 0;
-
-    digitalWrite(LEDPIN, HIGH);
-
-    // re-enable interrupt
-    attachInterrupt(digitalPinToInterrupt(DATAPIN), AcuriteSensor::interrupt_handler, CHANGE);
-  }
+ 
 };
 
 // #pragma once
